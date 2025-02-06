77feec791e3b87040705f91329dbe32901cec2c7
NgocDang2002 <truongngocdang0508@gmail.com>
2025-02-06 17:24:23 +0700

    moi

---
+++ b13_tao_giao_dien_ban_dau/android/app/src/main/kotlin/com/example/b13_tao_giao_dien_ban_dau/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.b13_tao_giao_dien_ban_dau
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lay_gia_tri_imput/android/app/src/main/kotlin/com/example/lay_gia_tri_imput/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lay_gia_tri_imput
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
